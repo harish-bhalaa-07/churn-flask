9f1258c586c2f085fae3bfcec4fe86c29efd2635
Harish Bhalaa <107175621+harish-bhalaa-07@users.noreply.github.com>
2023-11-16 16:11:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from flask import Flask, jsonify
+import numpy as np
+from flask import Flask,request,render_template
+import pickle
 import os
 
-app = Flask(__name__)
+os.chdir('./')
+
+app=Flask(__name__)
+model=pickle.load(open('churn.pkl','rb'))
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return render_template('churn_prediction.html')  #html page here
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    #storing the input values from html form
+    features=[[float(x) for x in request.form.values()]]
+    final_features=np.array(features)  
+
+    prediction=model.predict(final_features)
+    prediction=prediction.tolist()
+    
+    if prediction == [0]:
+        prediction = "You are a churn customer"
+    else:
+        prediction = "You are not a churn customer"
+
+    return render_template('churn_prediction.html', prediction_value = prediction)
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
